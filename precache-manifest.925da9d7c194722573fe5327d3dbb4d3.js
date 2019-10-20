b8bfe77a4d3bf739d265d856c3b119e9bcc42552
VerifiedFrame <56801030+VerifiedFrame@users.noreply.github.com>
2019-10-20 21:47:14 +0300

    1

---
+++ precache-manifest.925da9d7c194722573fe5327d3dbb4d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa6344d7972ebf0246e1d39c8004441",
+    "url": "/vk-list-date/index.html"
+  },
+  {
+    "revision": "efdc6d733e83039143dd",
+    "url": "/vk-list-date/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b417a75f70197501dd35",
+    "url": "/vk-list-date/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "efdc6d733e83039143dd",
+    "url": "/vk-list-date/static/js/2.268f9fe0.chunk.js"
+  },
+  {
+    "revision": "b417a75f70197501dd35",
+    "url": "/vk-list-date/static/js/main.a4265d21.chunk.js"
+  },
+  {
+    "revision": "76cc9623e3cc639163b5",
+    "url": "/vk-list-date/static/js/runtime-main.4f25538c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-list-date/static/media/persik.4e1ec840.png"
+  }
+]);
